cd09dfdbc897ca9c05fa37aaef8d10131a255b9c
ofigensky <bulletproofgleb@icloud.com>
2022-05-03 23:24:18 +0300

    наброска таймера в GameController и кнопки

--- ChallengeOne/Controllers/GameControllers.swift
+++ ChallengeOne/Controllers/GameControllers.swift
@@ -7,10 +7,37 @@
 import UIKit
 
 class GameController: UIViewController {
-
+    
+    var timer = Timer()
+    
+    var timeLeft = 10
+    
+    
+    @IBOutlet weak var timerTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         title = "Игра"
+        
+        timerFunc()
+        
     }
+    
+    func timerFunc() {
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            print("timer starts")
+            
+            self.timeLeft -= 1
+            
+            self.timerTextLabel.text = String(self.timeLeft)
+            print(self.timeLeft)
+            
+            if self.timeLeft == 0 {
+                timer.invalidate()
+            }
+        }
+    }
+    
 }
---
+++ ChallengeOne/Model/Questions.swift
@@ -0,0 +1,12 @@
+//
+//  Questions.swift
+//  ChallengeOne
+//
+//  Created by Gleb Glushok on 03.05.2022.
+//
+
+import Foundation
+
+struct Questions {
+    
+}
