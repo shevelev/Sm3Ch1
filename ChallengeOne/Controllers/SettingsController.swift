9b086134f3fda7cf06b6b6300da74a0f1aed8c92
ss <serejenka.shevelev@gmail.com>
2022-05-03 17:51:10 +0300

    привязал настройки экрана к коду.

--- ChallengeOne/Controllers/SettingsController.swift
+++ ChallengeOne/Controllers/SettingsController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class SettingsController: UIViewController {
 
+    @IBOutlet weak var comandCountLabel: UILabel!
+    @IBOutlet weak var wordToWin: UILabel!
+    @IBOutlet weak var roundTime: UILabel!
+    
+    @IBOutlet weak var wordSlider: UISlider!
+    @IBOutlet weak var timeSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "Настройки"
+        wordToWin.text = String(format: "%.0f", wordSlider.value)
+        roundTime.text = String(format: "%.0f", timeSlider.value)
     }
+    
+    
+    @IBAction func stepperPressed(_ sender: UIStepper) {
+        comandCountLabel.text = String(format: "%.0f", sender.value)
+    }
+    
+    @IBAction func wordSliderPressed(_ sender: UISlider) {
+        wordToWin.text = String(format: "%.0f", sender.value)
+    }
+    
+    @IBAction func timeRoundSliderPressed(_ sender: UISlider) {
+        roundTime.text = String(format: "%.0f", sender.value)
+    }
+    
 }
